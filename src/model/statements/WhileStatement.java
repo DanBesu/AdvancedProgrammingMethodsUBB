5b1e06a051fa1e1c805be221fa6b7a4dd6bb552f
Dan Constantin Besu <dbesu@cisco.com>
2021-11-22 23:29:16 +0200

    While Statement Added

---
+++ src/model/statements/WhileStatement.java
@@ -0,0 +1,38 @@
+package model.statements;
+
+import model.ADTs.IStack;
+import model.ProgramState;
+import model.exceptions.EvaluationException;
+import model.expressions.IExpression;
+import model.types.BoolType;
+import model.values.BoolValue;
+import model.values.IValue;
+
+public class WhileStatement implements IStatement{
+    IExpression expression;
+    IStatement statement;
+
+    public WhileStatement(IExpression expression, IStatement statement){
+        this.expression = expression;
+        this.statement = statement;
+    }
+
+    @Override
+    public ProgramState execute(ProgramState state) throws Exception {
+        IStack<IStatement> stack = state.getExecutionStack();
+        IValue expressionValue = expression.eval(state.getSymbolsDict());
+        if(!expressionValue.getType().equals(new BoolType())){
+            throw new EvaluationException("while condition is not a boolean");
+        }
+
+        if(((BoolValue) expressionValue).getValue()){
+            stack.push(this);
+            stack.push(statement);
+        }
+        return null;
+    }
+
+    public String toString(){
+        return "while (" + expression.toString() + ") {" + statement.toString() + "}";
+    }
+}
